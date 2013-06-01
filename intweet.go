1f466a49ffb43bcc03017fd2cf30dc3bd58e7f2c
Anders Pearson <anders@columbia.edu>
2013-06-01 00:27:00 -0400

    poll twitter in one thread, display results in the other.

--- intweet.go
+++ intweet.go
@@ -7,8 +7,10 @@ import (
 	"github.com/garyburd/go-oauth/oauth"
 	"github.com/xiam/twitter"
 	"io/ioutil"
+	"time"
 )
 
+// a "class" for tweets
 type tweet struct {
 	Handle   string
 	Text     string
@@ -28,11 +30,97 @@ func (t tweet) URL() string {
 	return fmt.Sprintf("https://twitter.com/%s/status/%s", t.Handle, t.Id)
 }
 
+// all the tweets that we know of.
+// maintain a slice of them ordered by date
+// and a map indexed by Id
+
+var MAX_TWEETS = 50
+
+type tweetCollection struct {
+	tweets []tweet
+	ids    map[string]tweet
+	chF    chan func()
+}
+
+func newTweetCollection() *tweetCollection {
+	t := &tweetCollection{
+		tweets: make([]tweet, 0),
+		ids:    make(map[string]tweet),
+		chF:    make(chan func()),
+	}
+	go t.backend()
+	return t
+}
+
+func (t *tweetCollection) backend() {
+	for f := range t.chF {
+		f()
+	}
+}
+
+func (t *tweetCollection) Add(tw tweet) {
+	t.chF <- func() {
+		_, present := t.ids[tw.Id]
+		if !present {
+			if len(t.tweets) >= MAX_TWEETS {
+				// first, throw away old ones
+				tt := make([]tweet, MAX_TWEETS)
+				copy(tt, t.tweets[len(t.tweets)-MAX_TWEETS:])
+				t.tweets = tt
+			}
+			t.tweets = append(t.tweets, tw)
+			t.ids[tw.Id] = tw
+		}
+	}
+}
+
+func (t tweetCollection) All() []tweet {
+	rch := make(chan []tweet)
+	go func() {
+		t.chF <- func() {
+			rch <- t.tweets
+		}
+	}()
+	return <-rch
+}
+
 type ConfigData struct {
 	ConsumerKey    string
 	ConsumerSecret string
 	OauthToken     string
 	OauthSecret    string
+}
+
+var POLL_INTERVAL = 60
+var DISPLAY_INTERVAL = 5
+
+func poll(client *twitter.Client, tweets *tweetCollection) {
+	for {
+		results, _ := client.HomeTimeline(nil)
+		for _, v := range *results {
+			var u map[string]interface{}
+			u = v["user"].(map[string]interface{})
+			t := tweet{
+				u["screen_name"].(string),
+				v["text"].(string),
+				v["created_at"].(string),
+				v["id_str"].(string),
+				u["name"].(string),
+			}
+			tweets.Add(t)
+		}
+		time.Sleep(time.Duration(POLL_INTERVAL) * time.Second)
+	}
+}
+
+func display(tweets *tweetCollection) {
+	for {
+		fmt.Println("----------------------")
+		for _, t := range tweets.All() {
+			fmt.Println(t.String())
+		}
+		time.Sleep(time.Duration(DISPLAY_INTERVAL) * time.Second)
+	}
 }
 
 func main() {
@@ -65,18 +153,8 @@ func main() {
 	if err != nil {
 		panic("error: " + err.Error())
 	}
-	results, err := client.HomeTimeline(nil)
-	for _, v := range *results {
-		var u map[string]interface{}
-		u = v["user"].(map[string]interface{})
-		t := tweet{
-			u["screen_name"].(string),
-			v["text"].(string),
-			v["created_at"].(string),
-			v["id_str"].(string),
-			u["name"].(string),
-		}
-		fmt.Println(t.String())
-		fmt.Println(t.URL())
-	}
+
+	tweets := newTweetCollection()
+	go poll(client, tweets)
+	display(tweets)
 }
